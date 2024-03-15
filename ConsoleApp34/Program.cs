41e90de85dd5d629cfc0a44a134147ed81c85c70
MirtalibEmirli <azad.sdk.85@icloud.com>
2024-03-15 17:23:10 +0400

    Add project files.

---
+++ ConsoleApp34/Program.cs
@@ -0,0 +1,148 @@
+﻿using System.Text.RegularExpressions;
+
+Console.WriteLine("Hello, World!");
+// ------------------------------------------------------------------------
+// Regex => 
+
+//string email = "asd+a!sd@gmail.com";
+
+//string regexPattern = "\\[a-zA-Z0-9\\]\\+@\\[a-zA-Z0-9\\._%\\+-\\]\\+\\\\\\.\\[a-zA-Z0-9\\._%\\+-\\]";
+
+//Regex regex = new Regex(regexPattern, RegexOptions.IgnoreCase);
+
+//if (regex.IsMatch(email))
+//    Console.WriteLine("Email is Okay");
+//else
+//    Console.WriteLine("Email is not Okay");
+
+
+
+// -------------------------------------------------------------------
+////var pattern = @"^\+994\s(55|50|99|77|70|10|51)\s\d{3}\s\d{2}\s\d{2}";
+//var pattern = @"^\+994\s\((55|50|99|77|70|10|51)\)\s\d{3}-\d{2}-\d{2}";
+
+//var phoneNumber = "+994 55 325-35-25";
+
+//if(Regex.IsMatch(phoneNumber, pattern))
+//{
+//    Console.WriteLine("Phone number is okay");
+//}
+//else
+//{
+//    Console.WriteLine("Phone number is not okay");
+//}
+
+
+
+
+// Create Password Regex patter. (minimum 8 characters and maximum 16 characters, at least one uppercase letter, one lowercase letter, one number and one special character)
+
+//var pattern = @"^(?=[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$";
+
+//var myPassword = "5aGGGGGV$i";
+
+//if(Regex.IsMatch(myPassword, pattern))
+//{
+//    Console.WriteLine("Password is okay");
+//}
+//else
+//{
+//    Console.WriteLine("Password is not okay");
+//}
+
+
+
+
+
+//string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+//string email = "c@gmail.com";
+
+//if(Regex.IsMatch(email, pattern))
+//{
+//    Console.WriteLine("Email is okay");
+//}
+//else
+//{
+//    Console.WriteLine("Email is not okay");
+//}
+
+// ------------------------------------------------------------------------------
+//// Local Function => Funksiya daxilinde funksya yaratmaqa deyilir. Yaradilan FUnction ancaq Haradadirsa orda istifade edile bilir.
+//MyMethod();
+
+
+//void MyMethod()
+//{
+//    Console.WriteLine("Hello, World!");
+//    Console.WriteLine("Hello, World!");
+//    Console.WriteLine("Hello, World!");
+//    Console.WriteLine("Hello, World!");
+//    Console.WriteLine("Hello, World!");
+//    Console.WriteLine("Hello, World!");
+//    Foo();
+//    Console.WriteLine("Hello, World!");
+//    Console.WriteLine("Hello, World!");
+//    Foo();
+//    Console.WriteLine("Hello, World!");
+//    Console.WriteLine("Hello, World!");
+//    Foo();
+//    void Foo()
+//    {
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//        Console.WriteLine("Hello, World!");
+//    }
+//}
+// --------------------------------------------------------------------------------------------
+// IlDasm => .Net Assembly-ni oxumaq ucun istifade olunur.
+Console.WriteLine("ASdasd");
+
+
+abstract class step {
+    const int a = 11;
+    readonly int b = 11;
+
+}
+static class code { }
+sealed class AAA { }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
